dc02bfe0107aa9cb80e40d66028c1514120732ee
UVP107 <unggulvalentinoputra3@gmail.com>
2026-02-06 15:45:48 +0700

    setup express.js & mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import { authenticate } from "./middlewares/auth.middlewares";
 
 const app = express();
 
@@ -8,7 +9,11 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("backend is running!");
+});
+
+app.get("/test-middleware", authenticate, (req, res) => {
+  res.send("endpoint bisa diakses public!");
 });
 
 export default app;
